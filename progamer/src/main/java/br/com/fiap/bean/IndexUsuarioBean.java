4d15a09b2ab19640decb92fb62ca4ff50e2735c0
HenriqueCoto <61854437+HenriqueCoto@users.noreply.github.com>
2021-04-30 20:15:47 -0300

    Add files via upload

---
+++ progamer/src/main/java/br/com/fiap/bean/IndexUsuarioBean.java
@@ -0,0 +1,12 @@
+package br.com.fiap.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class IndexUsuarioBean {
+	
+	public void execute() {
+		System.out.println("executando");
+	}
+	
+}
